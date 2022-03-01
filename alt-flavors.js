d890c7d40a63bd57ecf7aa5b1a6fe1c9862a6802
Nostro37 <nostro37@gmail.com>
2022-03-01 08:49:26 -0500

    exports set and linked

--- alt-flavors.js
+++ alt-flavors.js
@@ -1,1 +1,8 @@
-const items = ['item1', 'item2', 'item3', 'item4']
+module.exports.items = ['item1', 'item2', 'item3', 'item4'];
+
+const person = {
+    name: "bob", 
+
+}
+
+module.exports.singlePerson = person
--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const names = require('./names');
-const sayHi = require('./utils')
+const sayHi = require('./utils'); 
+const uncleBobby = require('./alt-flavors');
+require('./mind-grenade')
 
-sayHi(names.john);
-sayHi(names.paul);
+// sayHi(names.john);
+// sayHi(names.paul);
+// console.log(uncleBobby);
